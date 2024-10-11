b8f1adcf8e2282f2fc2be219e6a00b60718e6cb5
HuyNguyen <qhnguyen081100@gmail.com>
2024-10-11 15:19:06 +0700

    create the individual category screen

---
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import Button from "@/components/ui/button";
+import IconButton from "@/components/ui/icon-button";
+import { Color, Size } from "@/types"
+import { Dialog, DialogPanel } from "@headlessui/react";
+import { Plus, X } from "lucide-react";
+import { useState } from "react";
+import Filter from "./filter";
+
+interface MobileFitersProps {
+  sizes: Size[];
+  colors: Color[];
+};
+
+const MobileFiters = ({
+  sizes,
+  colors,
+}: MobileFitersProps) => {
+  const [open, setOpen] = useState(false);
+
+  const onOpen = () => setOpen(true);
+  const onClose = () => setOpen(false);
+  return (
+    <>
+      <Button onClick={onOpen} className="flex items-center gap-x-2 lg:hidden text-white">
+        Filters
+        <Plus size={20}/>
+      </Button>
+      <Dialog 
+        open={open} 
+        as="div"
+        className="relative z-40 lg:hidden"
+        onClose={onClose}
+      >
+        {/* Background */}
+        <div className="fixed inset-0 bg-black bg-opacity-25" />
+
+        {/* Dialog position */}
+        <div className="fixed inset-0 z-40 flex">
+          <DialogPanel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+            {/* Close button */}
+            <div className="flex items-center justify-end px-4">
+              <IconButton icon={<X size={15} />} onClick={onClose} />
+            </div>
+
+            {/* Render the filters */}
+            <div className="p-4">
+              <Filter 
+                valueKey="sizeId"
+                name="Sizes"
+                data={sizes}
+              />
+              <Filter 
+                valueKey="colorId"
+                name="Colors"
+                data={colors}
+              />    
+
+            </div>
+          </DialogPanel>
+        </div>
+      </Dialog>
+    </>
+  )
+}
+
+export default MobileFiters
